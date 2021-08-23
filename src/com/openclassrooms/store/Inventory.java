bad129f911e79e5df1bc1c7d6ebb6c7410566b7f
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2021-08-24 00:37:27 +0200

    Add the doc

--- src/com/openclassrooms/store/Inventory.java
+++ src/com/openclassrooms/store/Inventory.java
@@ -3,37 +3,55 @@ package com.openclassrooms.store;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Manage items and quantity of them Can display all items on the console
+ */
 public class Inventory {
 
-	public Map<Item,Integer> items = new HashMap<>();
-    
-    public Inventory() {
-    	items = new HashMap<>();
-    }
-    
-    public void addItem(Item itemToAdd, int quantity) {
-    	if (items.containsKey(itemToAdd)) {
-    		quantity=items.get(itemToAdd) + quantity;
-    	} 
-    	items.put(itemToAdd, quantity);
-    }
-    
-    public void removeItem(Item itemToRemove,int quantity) {
-    	if (items.containsKey(itemToRemove)) {
-    		quantity=items.get(itemToRemove)- quantity;
-    		if (quantity <=0) {
-    			items.remove(itemToRemove);
-    		}else {
-    			items.put(itemToRemove,quantity);
-    		}
-    		
-    	}
-    }
-    
-    public void displayInventoryOnConsole() {
-    	System.out.println(" --- Inventory --- ");
-        items.forEach((item, quantity) ->
-        System.out.println((item.getClass().getSimpleName() + " - " + item.brand + ":" + quantity)));
-    }
+	public Map<Item, Integer> items = new HashMap<>();
+
+	public Inventory() {
+		items = new HashMap<>();
+	}
+
+	/**
+	 * Add items to this inventory
+	 * 
+	 * @param itemToAdd Item to add to the inventory
+	 * @param quantity  Quantity to add
+	 */
+	public void addItem(Item itemToAdd, int quantity) {
+		if (items.containsKey(itemToAdd)) {
+			quantity = items.get(itemToAdd) + quantity;
+		}
+		items.put(itemToAdd, quantity);
+	}
+
+	/**
+	 * Remove items to this inventory
+	 * 
+	 * @param itemToRemove Item to remove from this inventory
+	 * @param quantity     Quantity to remove
+	 */
+	public void removeItem(Item itemToRemove, int quantity) {
+		if (items.containsKey(itemToRemove)) {
+			quantity = items.get(itemToRemove) - quantity;
+			if (quantity <= 0) {
+				items.remove(itemToRemove);
+			} else {
+				items.put(itemToRemove, quantity);
+			}
+
+		}
+	}
+
+	/**
+	 * Display on console the list of items in inventory and their quantity
+	 */
+	public void displayInventoryOnConsole() {
+		System.out.println(" --- Inventory --- ");
+		items.forEach((item, quantity) -> System.out
+				.println((item.getClass().getSimpleName() + " - " + item.brand + ":" + quantity)));
+	}
 
 }
--- src/com/openclassrooms/store/Item.java
+++ src/com/openclassrooms/store/Item.java
@@ -1,13 +1,16 @@
 package com.openclassrooms.store;
 
+/**
+ * Item that are managed by the inventory
+ */
 public class Item {
-	
-    protected Brand brand;
-    protected double price;
 
-    public Item(Brand brand, double price) {
-        this.brand = brand; // Updating brand field
-        this.price = price; // Updating price field
-    }
+	protected Brand brand;
+	protected double price;
+
+	public Item(Brand brand, double price) {
+		this.brand = brand; // Updating brand field
+		this.price = price; // Updating price field
+	}
 
 }
